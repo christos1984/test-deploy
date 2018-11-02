a914f50cf589ca2c102647e5d43e87a1a1f0824d
Chris Patsatzis <christos1984@gmail.com>
2018-11-02 17:30:24 +0100

    Added business logic

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
+require 'vendor/autoload.php';
 
-echo 'hi';
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+echo 'hi ' . getenv('INSTANCE');
+
+// Create connection
+$conn = new mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PWD'), getenv('MYSQL_DB'));
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT *  FROM accounts";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["account_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+
+?>
